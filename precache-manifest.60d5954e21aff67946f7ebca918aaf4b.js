1feea1111e692de16bacdb416364cb47dc40ffb7
Julie Hu <jhu21495@gmail.com>
2020-10-13 12:58:55 -0700

    Updates

---
+++ precache-manifest.60d5954e21aff67946f7ebca918aaf4b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b81381a2ca7d052bfda59c15a38f7a8",
+    "url": "/NailzbyAmyz/index.html"
+  },
+  {
+    "revision": "7d2eeb296800d20f3e00",
+    "url": "/NailzbyAmyz/static/css/main.a9a852dc.chunk.css"
+  },
+  {
+    "revision": "b55d21b6239fd9f828a0",
+    "url": "/NailzbyAmyz/static/js/2.bc4de67e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NailzbyAmyz/static/js/2.bc4de67e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d2eeb296800d20f3e00",
+    "url": "/NailzbyAmyz/static/js/main.b475270a.chunk.js"
+  },
+  {
+    "revision": "fc4c238fa8a581118423",
+    "url": "/NailzbyAmyz/static/js/runtime-main.30098f7a.js"
+  },
+  {
+    "revision": "6804dcb3a1f1de56c1aaad63577e6d31",
+    "url": "/NailzbyAmyz/static/media/aboutme.6804dcb3.jpg"
+  },
+  {
+    "revision": "d9a10890ee54ca44fbf8adcccabc3b2a",
+    "url": "/NailzbyAmyz/static/media/marble.d9a10890.jpg"
+  },
+  {
+    "revision": "fd56becdabf8e299a782ee8b7492209f",
+    "url": "/NailzbyAmyz/static/media/portfolio1.fd56becd.jpg"
+  },
+  {
+    "revision": "09585fbada7eade374c30aa890f2746c",
+    "url": "/NailzbyAmyz/static/media/portfolio2.09585fba.jpg"
+  },
+  {
+    "revision": "688e35017900a5334e5de0e632aee04c",
+    "url": "/NailzbyAmyz/static/media/portfolio3.688e3501.jpg"
+  },
+  {
+    "revision": "349d3bc5a693da33057a8f3a5e3e88f8",
+    "url": "/NailzbyAmyz/static/media/portfolio4.349d3bc5.jpg"
+  },
+  {
+    "revision": "6cafdef78f106009ba4c4eb58c2c6e5f",
+    "url": "/NailzbyAmyz/static/media/portfolio5.6cafdef7.jpg"
+  },
+  {
+    "revision": "706c33cd591543f0d3dac7d771f758cf",
+    "url": "/NailzbyAmyz/static/media/portfolio6.706c33cd.jpg"
+  },
+  {
+    "revision": "46561090ab24af96fbf0941a1d6e5e91",
+    "url": "/NailzbyAmyz/static/media/retina_wood_background.46561090.png"
+  }
+]);
